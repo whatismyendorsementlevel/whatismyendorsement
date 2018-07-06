6aa28f934c6bdc1c9f4859a45506a3337931f3a6
Karol Grabowski <karol.grabowski@allegro.pl>
2018-07-06 11:59:09 +0200

    another update

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import include, url
 from django.urls import path
 
 from django.contrib import admin
@@ -5,7 +6,15 @@ admin.autodiscover()
 
 import hello.views
 
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
 urlpatterns = [
     path('<platform>/<btag>', hello.views.index, name='index'),
-    path('*', hello.views.notFound, name='notFound')
+    path('*', hello.views.notFound, name='notFound'),
+    url(r'^$', hello.views.notFound, name='notFound'),
+    url(r'^*$', hello.views.notFound, name='notFound'),
+    url(r'^/*$', hello.views.notFound, name='notFound'),
+    url(r'*', hello.views.notFound, name='notFound'),
 ]
