71ea7259a094b310757ed97415153bcfcff0641c
Karol Grabowski <karol.grabowski@allegro.pl>
2018-07-06 14:11:19 +0200

    another update

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.urls import path
-
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 admin.autodiscover()
 
 import hello.views
@@ -10,3 +11,5 @@ urlpatterns = [
     path('<platform>/<btag>', hello.views.index, name='index'),
     # url(r'^.*$', hello.views.notFound, name='notFound'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
