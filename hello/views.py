103ee27ea73e649bfb3ccabed409f200e5f52fd9
Karol Grabowski <karol.grabowski@allegro.pl>
2018-07-06 10:19:01 +0200

    another update

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import urllib.request
 from bs4 import BeautifulSoup
-
-from .models import Greeting
 
 def index(request):
 
     page = urllib.request.urlopen('https://playoverwatch.com/pl-pl/career/pc/Artysta-2221')
-    print(page)
     soup = BeautifulSoup(page, 'html.parser')
     total = soup.find('svg', attrs={'class':'EndorsementIcon-border--shotcaller'})['data-total']
     shootcaller = soup.find('svg', attrs={'class':'EndorsementIcon-border--shotcaller'})['data-value']
@@ -16,12 +12,7 @@ def index(request):
     sportsmanship = soup.find('svg', attrs={'class':'EndorsementIcon-border--sportsmanship'})['data-value']
 
     endorsement = Endorsement(total, shootcaller, teammate, sportsmanship)
-    print("print")
-    # return HttpResponse(total)
-    # return render(endorsement, 'db.html')
-    return render(request, 'index.html', {'endorsement' : endorsement})
-
-
+    return render(request, 'index.html', {'endorsement': endorsement})
 
 class Endorsement(object):
     total = 0
@@ -34,12 +25,3 @@ class Endorsement(object):
         self.shootcaller = shootcaller
         self.sportsmanship = sportsmanship
         self.teammate = teammate
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
