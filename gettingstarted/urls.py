cb440aedc8d401470dda6f8479c857f192309b09
Karol Grabowski <karol.grabowski@allegro.pl>
2018-07-06 15:11:50 +0200

    another update

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -9,7 +9,7 @@ import hello.views
 
 urlpatterns = [
     path('<platform>/<btag>', hello.views.index, name='index'),
-    # url(r'^.*$', hello.views.notFound, name='notFound'),
+    url(r'^.*$', hello.views.notFound, name='notFound'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
