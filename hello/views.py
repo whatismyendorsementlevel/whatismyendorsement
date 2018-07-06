4014520992eec3ea3ea17cf81bc31ca76fb2e821
Karol Grabowski <karol.grabowski@allegro.pl>
2018-07-06 11:35:35 +0200

    another update

--- hello/views.py
+++ hello/views.py
@@ -4,25 +4,28 @@ from bs4 import BeautifulSoup
 
 def index(request, platform, btag):
 
-    # validate platform, return 404 when 404, css
+    # validate platform, return 404 when 404, css, obecne przewitywane progi
     soup = BeautifulSoup(urllib.request.urlopen('https://playoverwatch.com/pl-pl/career/' + platform + '/' + btag), 'html.parser')
 
     total = soup.find('svg', attrs={'class':'EndorsementIcon-border--shotcaller'})['data-total']
+    level = soup.find('div', attrs={'class':'u-center'})
     shootcaller = soup.find('svg', attrs={'class':'EndorsementIcon-border--shotcaller'})['data-value']
     teammate = soup.find('svg', attrs={'class':'EndorsementIcon-border--teammate'})['data-value']
     sportsmanship = soup.find('svg', attrs={'class':'EndorsementIcon-border--sportsmanship'})['data-value']
 
-    endorsement = Endorsement(total, shootcaller, teammate, sportsmanship)
-    return render(request, 'index.html', {'endorsement': endorsement}, {'request': request.build_absolute_uri()})
+    endorsement = Endorsement(total, level, shootcaller, sportsmanship, teammate)
+    return render(request, 'index.html', {'endorsement': endorsement})
 
 class Endorsement(object):
     total = 0
+    level = 0
     shootcaller = 0
     sportsmanship = 0
     teammate = 0
 
-    def __init__(self, total, shootcaller, sportsmanship, teammate):
+    def __init__(self, total, level, shootcaller, sportsmanship, teammate):
         self.total = total
+        self.level = level
         self.shootcaller = shootcaller
         self.sportsmanship = sportsmanship
         self.teammate = teammate
