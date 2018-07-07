1fa836f7586a3cc8a3cc87ffb3dedf19fbb95e4a
Karol Grabowski <karol@Karol.local>
2018-07-07 17:23:57 +0200

    hi btag

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -1,10 +1,11 @@
+#-*- coding: utf-8 -*-
 import os
 import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DEFAULT_CHARSET = 'utf-8'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+from django.utils.encoding import smart_str
 from django.shortcuts import render
 import urllib.request
 from bs4 import BeautifulSoup
@@ -9,13 +11,18 @@ def index(request, platform, btag):
     # 500 if no endorce at all
     # accept btags with hash
 
+
+
     if platform not in ['pc', 'psn', 'xbl']:
         return notFound(request)
+# testing below
+    person = Person(btag)
+    endorsement = Endorsement(1,1,1,1,1)
+    return render(request, 'index.html', {'endorsement': endorsement, 'person': person})
+# testing above
 
-    urlopen = urllib.request.urlopen('https://playoverwatch.com/pl-pl/career/' + platform + '/' + btag)
-
+    person = Person(btag)
     soup = BeautifulSoup(urlopen, 'html.parser')
-
 
     isOK = soup.find('div', attrs={'class':'u-center'})
     if isOK is None:
@@ -29,7 +36,7 @@ def index(request, platform, btag):
 
     endorsement = Endorsement(total, level, shotcaller, sportsmanship, teammate)
 
-    return render(request, 'index.html', {'endorsement': endorsement})
+    return render(request, 'index.html', {'endorsement': endorsement, 'person': person})
 
 
 def getTotal(soup):
@@ -52,7 +59,7 @@ def getTeammate(soup):
         return 0
     else:
         return total['data-value']
-        
+
 def getSportsmanship(soup):
     total = soup.find('svg', attrs={'class':'EndorsementIcon-border--sportsmanship'})
     if total is None:
@@ -73,6 +80,11 @@ class Endorsement(object):
         self.shotcaller = shotcaller
         self.sportsmanship = sportsmanship
         self.teammate = teammate
+
+class Person(object):
+    btag = ""
+    def __init__(self, btag):
+        self.btag = btag
 
 def notFound(request):
     return render(request, '404.html')
